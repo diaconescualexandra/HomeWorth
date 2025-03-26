fcbb7f4735bb5337d1301f4ab237de9fb239b827
Alexandra Diaconescu <alexandraisi46@gmail.com>
2025-03-26 22:50:57 +0200

    configure routes & cleanup

--- HomeWorth.Client/src/app/app.component.ts
+++ HomeWorth.Client/src/app/app.component.ts
@@ -1,25 +1,26 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { HomeService } from './services/home.service';
 import { CommonModule } from '@angular/common';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  title = 'HomeWorth.Client';
-  homeMessage: string = ''; // Variable to hold the API response
+export class AppComponent {
+  // title = 'HomeWorth.Client';
+  // homeMessage: string = ''; // Variable to hold the API response
 
-  homeService = inject(HomeService);
+  // homeService = inject(HomeService);
 
-  ngOnInit(): void {
-    this.homeService.get().subscribe(
-      (response) => {
-        this.homeMessage = response.message;  // Get message from response
-      }
-    );
-  }
+  // ngOnInit(): void {
+  //   this.homeService.get().subscribe(
+  //     (response) => {
+  //       this.homeMessage = response.message;  // Get message from response
+  //     }
+  //   );
+  // }
 }
