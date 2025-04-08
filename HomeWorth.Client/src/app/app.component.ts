b0a8ecf08872b7c9e597aeb2e417a29e6d986add
Alexandra Diaconescu <alexandraisi46@gmail.com>
2025-04-08 15:23:10 +0300

    update

--- HomeWorth.Client/src/app/app.component.ts
+++ HomeWorth.Client/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { HomeService } from './services/home.service';
 import { CommonModule } from '@angular/common';
 import { Router } from 'express';
+import { HeaderComponent } from "./pages/home/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule,RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
