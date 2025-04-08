862073ac0c0b5e8ec81b7c82c3f3ebaa7f0801a7
Alexandra Diaconescu <alexandraisi46@gmail.com>
2025-04-08 15:21:00 +0300

    setup for jwt and backend integration

--- HomeWorth.Client/src/app/app.routes.ts
+++ HomeWorth.Client/src/app/app.routes.ts
@@ -1,9 +1,42 @@
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
-
+import { AuthGuardService } from './services/auth-guard.service';
+import { HeaderComponent } from './pages/home/header.component';
+import { AddPropertyComponent } from './pages/properties/add-property.component';
 export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: 'register', component: RegisterComponent },
-    { path: 'login', component: LoginComponent }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'add-property', component: AddPropertyComponent },
+  
+  // Seller routes
+  { 
+//     path: 'my-properties', 
+//     //component: PropertiesComponent, 
+//     canActivate: [AuthGuardService],
+//     data: { roles: ['Seller'] }
+//   },
+//   { 
+    path: 'add-property', 
+    component: AddPropertyComponent, 
+    canActivate: [AuthGuardService],
+    data: { roles: ['Seller'] }
+  },
+
+//   // Buyer routes
+//   { 
+//     path: 'favorites', 
+//     //component: FavoritesComponent, 
+//     canActivate: [AuthGuardService],
+//     data: { roles: ['Buyer'] }
+//   },
+
+//   // Admin routes
+//   { 
+//     path: 'admin', 
+//     //loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), 
+//     canActivate: [AuthGuardService],
+//     data: { roles: ['Admin'] }
+//   }
 ];
